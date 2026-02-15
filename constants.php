1316f9e4675fa650ff68b70ab49c0df8fda47582
Saurabh022mishra <skm80050@gmail.com>
2026-02-15 22:56:31 +0530

    CONSTANT

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+// Defining constants
+define("PI", 3.14159);
+define("GRAVITY", 9.81);
+define("SPEED_OF_LIGHT", 299792458);
+
+// Using constants
+echo "Value of PI: " . PI . "\n";
+echo "Value of Gravity: " . GRAVITY . "\n";
+echo "Value of Speed of Light: " . SPEED_OF_LIGHT . "\n";
+?>
