3cdbf8a4aa411d27e62c4cfb17dc2915a5cdb46c
Saurabh022mishra <skm80050@gmail.com>
2026-02-21 22:29:23 +0530

    Typecasting

---
+++ variable_typecasting.php
@@ -0,0 +1,10 @@
+//Write a Program in PHP for type Casting Of a Variables
+<?php
+$var = "10.5";
+$intVar = (int)$var;
+$floatVar = (float)$var;
+
+echo "Original Value: " . $var . "\n";
+echo "Integer Value: " . $intVar . "\n";
+echo "Float Value: " . $floatVar . "\n";    
+?>
