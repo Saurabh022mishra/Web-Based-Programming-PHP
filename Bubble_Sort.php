508bbed17a3ef78acabd608072b3c27f8c46c293
Saurabh022mishra <skm80050@gmail.com>
2026-02-23 21:05:42 +0530

    Bubble Sort

--- Bubble_Sort.php
+++ Bubble_Sort.php
@@ -16,12 +16,19 @@ print_r($array);
 
 /* Bubble Sort */
 for ($i = 0; $i < $n - 1; $i++) {
+    echo "Pass " . ($i + 1) . ":\n";
     for ($j = 0; $j < $n - $i - 1; $j++) {
+        echo "Comparing " . $array[$j] . " and " . $array[$j + 1] . "\n";
+        echo "jvalue: " . $array[$j] . ", j+1 value: " . $array[$j + 1] . "\n";
         if ($array[$j] > $array[$j + 1]) {
+            echo "Swapping " . $array[$j] . " and " . $array[$j + 1] . "\n";
             $temp = $array[$j];
+
             $array[$j] = $array[$j + 1];
+
             $array[$j + 1] = $temp;
         }
+echo"\n";
     }
 }
 
