0aa4aa978c84e528620652b2595669f1cbdd7fe6
Saurabh022mishra <skm80050@gmail.com>
2026-03-12 22:42:51 +0530

    File Downloading

---
+++ download_file.php
@@ -0,0 +1,13 @@
+<?php
+$file = 'example.txt';
+
+if (file_exists($file)) {
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="' . basename($file) . '"');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+} else {
+    echo "File not found.";
+}
+?>
